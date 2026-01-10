275c67a8255dd46d87c27b0b5e03af067c7bb674
alderjhuynh <alderjhuynh@gmail.com>
2026-01-10 01:46:19 -0800

    Update level4.js

--- levels/level4.js
+++ levels/level4.js
@@ -3,12 +3,12 @@ import { Vector3 } from "three";
 export const level4 = {
   id: 4,
   name: "Level 4: Climb & Dash",
-  spawn: new Vector3(0, 1.6, 6),
+  spawn: { position: new Vector3(0, 1.6, 6), rotationY: 0 },  
   build({ clearObstacles, createObstacle, createBoxObstacle, createBounds }) {
     clearObstacles();
     createBounds(24, 12, 1);
     createBoxObstacle(0, 35, 4, 4, 16.5, 1.4, { climbable: true, wCenter: 0, wSize: Infinity });
-    createBoxObstacle(0, 20, -1, 4, 28, 1.4, { climbable: true, wCenter: 0, wSize: Infinity });
+    createBoxObstacle(0, 20.2, -3, 4, 28, 1.4, { climbable: true, wCenter: 0, wSize: Infinity });
     createBoxObstacle(0, 46.5, -1, 6, 2, 6, { climbable: true, wCenter: 0, wSize: Infinity });
     createBoxObstacle(0, 0, 0, 2.6, 20.2, 2.6, { climbable: true, wCenter: 0, wSize: Infinity });
   }
